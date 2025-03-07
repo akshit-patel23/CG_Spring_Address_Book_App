253c7178c41dd263c692fa40d6cb578429bc2153
Akshit Patel <akshitpatel.g@gmail.com>
2025-03-07 12:43:04 +0530

    [Akshit] Add . code for AddressBookDTO

---
+++ src/main/java/com/example/addressbook/dto/AddressBookDTO.java
@@ -0,0 +1,37 @@
+package com.example.addressbook.dto;
+
+public class AddressBookDTO {
+    private String name;
+    private String address;
+    private int id;
+
+    public AddressBookDTO() {} // Default constructor
+
+    public AddressBookDTO(String name, String address,int id) {
+        this.name = name;
+        this.address = address;
+        this.id=id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+}
